1a8caa191180cd033cbba7861e2f3d659d04493a
ThatFatPat <idoshav@gmail.com>
2019-10-08 10:10:08 +0300

    Use pyodbc, fix driver error.

--- FlaskWeb/__init__.py
+++ FlaskWeb/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import pymssql as sql
+import pyodbc as sql
 import hashlib
 import json
 
@@ -31,17 +31,19 @@ extended_cols = settings["extend_cols"]
 types = "',N'".join(map(str, settings["object_types"])) # Join types to a comma-seperated string.
 types = "(N'" + types + "')" # Output format is "N'U',N'V',N'P'"
 
-def getExtendedProperty(cursor, property):
+def getExtendedProperty(conn, property):
     query = "SELECT " + name_col_selector + ''', value
             FROM sys.extended_properties
             INNER JOIN sys.objects ON sys.extended_properties.major_id=sys.objects.OBJECT_ID AND class=1
             WHERE sys.extended_properties.name = '{}' '''.format(property)
+    conn.add_output_converter(-150, lambda x: x.decode('utf-8')) # Handle binary string coming from extended_properties.
+    cursor = conn.cursor()
     cursor.execute(query)
     return cursor.fetchall()
 
 def getItemsFromSQL():
     for database in settings["databases"]:
-        conn = sql.connect(server="sql5045.site4now.net", database="DB_A4E964_MSSQL", user="DB_A4E964_MSSQL_admin", password="ZP21ya10")
+        conn = sql.connect(database)
         cursor = conn.cursor()
 
         # Note: This code is bad form! Please avoid using format strings for queries.
@@ -51,11 +53,11 @@ def getItemsFromSQL():
 
         rows = cursor.fetchall()
         dict_rows = [dict(zip(sql_col_names, list(row))) for row in rows]
-        extended_property_dicts = [{"col": col["desc"], "dict": dict(getExtendedProperty(cursor, col["id"]))} for col in extended_cols]
+        extended_property_dicts = [{"col": col["desc"], "dict": dict(getExtendedProperty(conn, col["id"]))} for col in extended_cols]
         for row in dict_rows:
             for prop_dict in extended_property_dicts:
                 if row["Name"] in prop_dict["dict"]:
-                    row[prop_dict["col"]] = prop_dict["dict"][row["Name"]].decode('utf-8')
+                    row[prop_dict["col"]] = prop_dict["dict"][row["Name"]]
                 else:
                     row[prop_dict["col"]] = ""
         conn.close()
@@ -63,7 +65,7 @@ def getItemsFromSQL():
 
 @app.route('/')
 def displayTable():
-    return render_template('index.html', items=getItemsFromSQL(), columns=sql_col_names+extended_col_names)
+    return render_template('index.html', items=getItemsFromSQL(), columns=sql_col_names + extended_col_names)
 
 # if __name__ == '__main__':
 #     app.run()
